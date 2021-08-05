33edba6493d7a9174d22077cadab03818238e688
Ricki Chandra Hidayatullah <richan@Fawwazzzs-Air.localdomain>
2021-08-05 10:49:49 +0700

    add server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const contentRoute = require('')
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+// app.use(contentRoute);
+
+app.get('/hello',(req,res) => {
+    console.log('im ready')
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
